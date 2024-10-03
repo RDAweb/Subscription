8c62141671a9800da67d98774e7e67473a6d51c2
rg <ravindergod001tbdl@gmail.com>
2024-10-03 19:49:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('#header', { duration: 1500, origin: 'bottom', distance: '50px' });
+        ScrollReveal().reveal('.plan', { duration: 1000, interval: 200, scale: 0.8 });
+        ScrollReveal().reveal('.benefit-item', { duration: 1000, interval: 200, origin: 'left', distance: '50px' });
+        ScrollReveal().reveal('#cta', { duration: 1000, scale: 0.9 });
